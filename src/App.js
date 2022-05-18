ee51646fc44e427c564553454d6755dcea607fc8
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-05-17 20:18:10 -0400

    add dropdown component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Dropdown from './components/Dropdown';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Dropdown />
     </div>
   );
 }
