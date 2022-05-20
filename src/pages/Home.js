3a68626aa3aaff9b8ea366f8e3d444c836b26e54
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-05-20 14:35:47 -0400

    Add Search Feature

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { GiChart } from 'react-icons/gi';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import CategoryCard from '../components/CategoryCard';
 import '../styles/home.css';
 
@@ -14,13 +15,21 @@ const Home = () => {
     (state) => state.stonks.stonks,
   );
 
+  const categories = stonks.filter((stonk) => catList.includes(stonk.misc.category));
+
+  const [categoryList, setList] = useState(categories);
+
   const navigate = useNavigate();
 
   const stonksText = `Total Stonks: ${stonks.length}`;
 
   const handleRedirect = (category) => `/${category}`;
 
-  const categories = stonks.filter((stonk) => catList.includes(stonk.misc.category));
+  const handleChange = (e) => {
+    setList(categories.filter(
+      (stonk) => stonk.misc.category.toLowerCase().includes(e.toLowerCase()),
+    ));
+  };
 
   return (
     <>
@@ -37,10 +46,17 @@ const Home = () => {
         </div>
         <div className="categoryCardContainer">
           <span className="filterText">Stonks by category</span>
+          <input onChange={(e) => handleChange(e.target.value)} type="text" />
           <ul className="categoryList">
-            {categories.map((stonk) => (
+            {categoryList.length ? categoryList.map((stonk) => (
               <li className="categoryLI" key={stonk.misc.category}>
-                <button className="categoryCard" onClick={() => { navigate(handleRedirect(stonk.misc.category)); }} type="button">
+                <button className="categoryCard" onClick={() => { navigate(handleRedirect(stonk.misc.category.toLowerCase())); }} type="button">
+                  <CategoryCard category={stonk.misc.category} />
+                </button>
+              </li>
+            )) : categories.map((stonk) => (
+              <li className="categoryLI" key={stonk.misc.category}>
+                <button className="categoryCard" onClick={() => { navigate(handleRedirect(stonk.misc.category.toLowerCase())); }} type="button">
                   <CategoryCard category={stonk.misc.category} />
                 </button>
               </li>
